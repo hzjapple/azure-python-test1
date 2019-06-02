3eb56a56a2b071ba49ddc8bdbc67225cd77bb319
linda-han <linda.han@pwc.com>
2019-06-02 10:36:14 +0800

    test2

--- insta_project/urls.py
+++ insta_project/urls.py
@@ -21,7 +21,6 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
