8f238ee5ae0737c24e4f6822eaa64cb02c03ca58
linda-han <linda.han@pwc.com>
2019-06-02 11:06:17 +0800

    更改sqllit3的路径

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'insta_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/var/opt/db.sqlite3',
     },
     'OPTIONS': {
         'timeout': 200,
